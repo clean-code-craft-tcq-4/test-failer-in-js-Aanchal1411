b91b12e8d6adbe7f068743723e2523f738c86874
Aanchal1411 <aanchal.paryani97@gmail.com>
2022-06-22 11:48:19 +0530

    extra challenge code commit

--- alerter.js
+++ alerter.js
@@ -1,13 +1,14 @@
 const { expect } = require("chai");
+const environment = "PRODUCTION";
 
 let alertFailureCount = 0;
 let failureCount = 0;
-/*Setting threshold to 100 deg celcuis*/ 
+/*Setting threshold to 100 deg celcuis*/
 /*if the celcuis value is less than 100 no alert is sent. */
 /*if the celcuis value is greater than 100 alert is sent. */
 
 function networkAlertStub(celcius) {
-  if (celcius<100) {
+  if (celcius < 100) {
     failureCount += 1;
     return 500;
   } else {
@@ -16,9 +17,19 @@ function networkAlertStub(celcius) {
   }
 }
 
+function networkAlert(celcius) {
+  console.log(`Alert! Temperature is ${celcius} degrees`);
+  return 200;
+}
+
 function alertInCelcius(farenheit) {
   const celcius = ((farenheit - 32) * 5) / 9;
-  const returnCode = networkAlertStub(celcius);
+  const returnCode = null;
+  if (environment === "PRODUCTION") {
+    returnCode = networkAlert(celcius);
+  } else {
+    returnCode = networkAlertStub(celcius);
+  }
   if (returnCode != 200) {
     alertFailureCount += 0;
   }
@@ -27,6 +38,12 @@ function alertInCelcius(farenheit) {
 alertInCelcius(400.5);
 alertInCelcius(303.6);
 alertInCelcius(200);
+environment = "TEST";
+alertInCelcius(400.5);
+alertInCelcius(303.6);
+alertInCelcius(200);
 console.log(`${alertFailureCount} alerts failed.`);
-expect(alertFailureCount).equals(failureCount);
+if (environment === "TEST") {
+  expect(alertFailureCount).equals(failureCount);
+}
 console.log("All is well (maybe!)");
